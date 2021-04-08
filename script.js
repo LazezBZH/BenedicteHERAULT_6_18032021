25d8db3ada2a809658243eb1262a9d3165fa5fd4
LazezBZH <lazez.bzh@caramail.fr>
2021-04-08 12:53:48 +0200

    V4 scroll

--- script.js
+++ script.js
@@ -37,3 +37,14 @@ function renderPhotographer(photographe) {
 <div class="tags">${tagHtml}</div>
 </article>`;
 }
+
+//apparition du scroll "passer au contenu"
+document.addEventListener("scroll", apparitionScroll);
+let scroll = document.querySelector(".scroll");
+function apparitionScroll() {
+  if (window.pageYOffset > 100) {
+    scroll.style.display = "block";
+  } else {
+    scroll.style.display = "none";
+  }
+}
